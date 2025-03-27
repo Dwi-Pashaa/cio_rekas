d1d641606013d82ecbec194917267fbef676b3b4
DwiPashaDjango <dwipasha336@gmail.com>
2025-03-27 15:34:44 +0700

    Update Transaction Chart

--- resources/views/pages/transaction/chart/index.blade.php
+++ resources/views/pages/transaction/chart/index.blade.php
@@ -54,7 +54,7 @@
             var customerChart = new ApexCharts(document.getElementById('chart-top-customer'), {
                 chart: {
                     type: "bar",
-                    height: 1000,
+                    height: "100%",
                     toolbar: { show: false }
                 },
                 plotOptions: {
