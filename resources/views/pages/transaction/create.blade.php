7a2a206fa3283435a124274a300cc067fcc82f50
DwipashaDjango <dwipasha336@gmail.com>
2025-04-10 08:50:37 +0700

    Update Scanner

--- resources/views/pages/transaction/create.blade.php
+++ resources/views/pages/transaction/create.blade.php
@@ -182,15 +182,6 @@
                     }
                 }).fail(function(jqXHR, textStatus, errorThrown) {
                     console.log("Error:", textStatus, errorThrown);
-                }).always(function () {
-                    qrScanner.stop().then(() => {
-                        document.getElementById("reader").innerHTML = "";
-                        startQRScanner(currentCameraId);
-                    });
-                });
-
-                qrScanner.stop().then(() => {
-                    document.getElementById(qrCodeRegionId).innerHTML = "";
                 });
             };
 
